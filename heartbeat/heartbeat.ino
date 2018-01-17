f9c454552469307c8b298c6423166e44fcb435d7
ryant <ryant@DESKTOP-UHKTFSQ.dhcp.wustl.edu>
2018-01-17 15:21:45 -0600

    Heartbeat

---
+++ heartbeat/heartbeat.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  delay(500);
+  digitalWrite(13, HIGH);
+  int x = millis() / 1000;
+  delay(500);
+  digitalWrite(13, LOW);
+  Serial.print(x);
+  Serial.println(" sec have elapsed");
+}
+
